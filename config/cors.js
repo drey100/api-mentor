717b73e6ffd260649e6115e946e6187bbbdf5e90
Idriss98-i <idrissoubachabi781@gmail.com>
2025-04-22 12:16:50 +0100

    Message de commit

--- config/cors.js
+++ config/cors.js
@@ -1,8 +1,21 @@
+// config/cors.js
 const cors = require('cors');
 
+const allowedOrigins = [
+  'http://localhost:5173', // Frontend en développement
+];
+
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      // Si l'origine est dans la liste autorisée ou si la requête est sans origine (par exemple, Postman)
+      callback(null, true);
+    } else {
+      // Rejetter la requête si l'origine n'est pas autorisée
+      callback(new Error('CORS policy: Origin not allowed'), false);
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
