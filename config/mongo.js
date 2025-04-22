abd52d28f7f2ac88b682c10307c49c0eddd41315
Idriss98-i <idrissoubachabi781@gmail.com>
2025-04-22 12:48:06 +0100

    Fix register API

--- config/mongo.js
+++ config/mongo.js
@@ -1,16 +1,19 @@
-// config/mongo.js
 const mongoose = require('mongoose');
-require('dotenv').config();
 
-const mongoURI = process.env.MONGO_URI_LOCAL;
-
-async function connectMongo() {
-  try {
-    await mongoose.connect(mongoURI); // plus besoin de passer les options
-    console.log(' Connexion à MongoDB locale réussie !');
-  } catch (error) {
-    console.error(' Erreur de connexion à MongoDB :', error.message);
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
   }
-}
+});
 
-module.exports = connectMongo;
+module.exports = mongoose.model('User', userSchema);
